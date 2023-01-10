8083f32d65a11a7f9670f0d199b7703846597466
Marcio Orladini <morlandini1@gmail.com>
2023-01-10 10:33:38 -0300

    MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Widget());
+ runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement createElement
-    throw UnimplementedError();
+  Widget build(BuildContext context){
+    return MaterialApp();
   }
-  
-
 }
