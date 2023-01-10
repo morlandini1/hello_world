8b385fa07a24a413c14711680eee34aa961aa377
Marcio Orladini <morlandini1@gmail.com>
2023-01-10 10:14:48 -0300

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,15 @@
-void main() {}
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(Widget());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement createElement
+    throw UnimplementedError();
+  }
+  
+
+}
