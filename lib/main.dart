b716895caf8cc085b6192ff534cb48da8a976671
Marcio Orladini <morlandini1@gmail.com>
2023-01-11 12:23:19 -0300

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
- runApp(MyApp());
+ runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: HomePage(),
     );
   }
 }
 
   class HomePage extends StatelessWidget {
+   const HomePage({super.key});
+
      @override
   Widget build(BuildContext context){
-    return MaterialApp(
-      home: Material(
-        child: Center(
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+        body: Center(
           child: Text('Flutterando'),
           ),
-      ));
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+            
+          
+            },
+          ),
+      );
   }
 }
