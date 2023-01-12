d4bff51dde046dd6fb1aa9d928aa6a2e979877d0
Marcio Orladini <morlandini1@gmail.com>
2023-01-12 10:23:12 -0300

    InheritedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/myapp.dart';
 
 void main() {
  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
-    );
-  }
-}
 
-  class HomePage extends StatelessWidget {
-   const HomePage({super.key});
 
-     @override
-  Widget build(BuildContext context){
-    return  Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-        body: Center(
-          child: Text('Flutterando'),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () {
-              print('clicou');
-            
-          
-            },
-          ),
-      );
-  }
-}
